0c25a68da3d486ff60cf1157e4a22a8977b34ddd
Eragon35 <antipin35@yahoo.com>
2021-03-09 22:41:06 +0300

    Add some logic

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,36 @@
-
+import scala.io.StdIn
+import scala.math
 
 object Main {
+  var func = null // TODO: rework to smth normal
+  var left: Double = 0
+  var right: Double = 0
 
   def main(args: Array[String]): Unit = {
-    println("Вариант №2\n" +
-      "Метод половинного деления\n" +
-      "Метод секущих\n" +
-      "Метод простой итерации\n" +
-      "Заданная функция: −1,38x^3 − 5,42x^2 + 2,57x + 10,95")
+    println(""" Вариант №2
+      | Метод половинного деления
+      | Метод секущих
+      | Метод простой итерации
+      | Заданная функция: −1,38x^3 − 5,42x^2 + 2,57x + 10,95""".stripMargin)
 
     while (true) {
+      println("\nfunctions" + "\nВыберите функцию: ")
+      func = ConsoleHandler.functionHandler(StdIn.readLine())
+      // show graph of function
+      var confirm = false
+      while (!confirm) {
+        println("Введите левый интервал")
+        left = StdIn.readDouble()
+        println("Введите правый интервал")
+        right = StdIn.readDouble()
+        // show new graph
+        println("Вас устраивает график? Он содержит корни")
+        confirm = ConsoleHandler.agreeHandler(StdIn.readLine())
+        // check if roots exist
+      }
+      // find right root by 'Метод половинного деления'
+      // find left root by 'Метод секущих'
+      // find central root by 'Метод простой итерации'
 
     }
   }
